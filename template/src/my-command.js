f17b7d97970a285d9843fd3ee7f9f789aa3da682
Ale M <bomberstudios@gmail.com>
2021-01-18 13:35:53 +0100

    Added `public.json` option

--- template/src/my-command.js
+++ template/src/my-command.js
@@ -6,6 +6,7 @@ export function onStartup () {
   // To register the plugin, uncomment the relevant type:
   DataSupplier.registerDataSupplier('public.text', '{{ name }}', 'SupplyData')
   // DataSupplier.registerDataSupplier('public.image', '{{ name }}', 'SupplyData')
+  // DataSupplier.registerDataSupplier('public.json', '{{ name }}', 'SupplyData') // Available in Sketch 71+
 }
 
 export function onShutdown () {
@@ -18,6 +19,15 @@ export function onSupplyData (context) {
   const items = util.toArray(context.data.items).map(sketch.fromNative)
   items.forEach((item, index) => {
     let data = Math.random().toString()
+    // For `public.image`, `data` must be a path to an image
+    // let data = 'path/to/image'
+    // For `public.json`, `data` must be a JSON object with layer names for keys, and content for values
+    // let data = {
+    //   "name": "John",
+    //   "surname": "Doe",
+    //   "email": "developer@sketch.com"
+    // }
+    // Check the docs for more info: https://developer.sketch.com/reference/api/#data-supplier
     DataSupplier.supplyDataAtIndex(dataKey, data, index)
   })
 }
